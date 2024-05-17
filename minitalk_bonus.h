65a2ebf90be478318809f04069a938bf44aebf4f
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-05-17 16:09:37 +0200

    added minitalk_bonus.h

---
+++ minitalk_bonus.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 15:37:54 by kdvarako          #+#    #+#             */
+/*   Updated: 2024/05/17 15:38:35 by kdvarako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <signal.h>
+# include "./libft/libft.h"
+
+#endif
